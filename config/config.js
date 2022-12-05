5934869a152c952e20bb4a971973deec788f8a57
clambert-ign <craig.lambert@ignifi.co.uk>
2022-12-05 10:44:31 +0000

    Changes

--- config/config.js
+++ config/config.js
@@ -18,8 +18,6 @@ config.acoustic = {
   //Live Acoustic subscription
   //contentHubID: '7dd72924-538f-4876-a2c6-b384c797bccb',
   //UAT Acoustic subscription
-  //contentHubID: 'fadaa259-5193-4c1e-984b-5d76502c7dcb', 
-  //CTCL UAT   
   contentHubID: 'c21df77d-c3dd-42a7-837d-0d360900e599',
   searchPath: '/delivery/v1/search?',
   searchQueryPath: '/search?',
@@ -27,8 +25,6 @@ config.acoustic = {
   deliveryPath: '/delivery/v1',
   renderContextPath: '/rendering',
   libraries: activeLibraries,
-  //defaultHomePageId: 'b4583b8b-ed99-4aae-b722-b9e6340a74ec'
-  //CTCL Homepage Id
   defaultHomePageId: '9fb6b25f-7d40-49e7-b0e4-287e2e35e992'
 }
 
@@ -42,9 +38,8 @@ config.homepageId = (uuid) => {
   return homepageId
 }
 
-config.theme = 'theme-kanso-primary'
+config.theme = 'theme-ctcl'
 config.languageInUse = ''
-
 
 // NProgress
 config.nProgress = {
@@ -61,21 +56,6 @@ config.basicAuth = {
     user: 'preview', 
     password: 'i9n1f1d3v' 
   }]
-}
-
-// Cookie Settings
-config.cookie = {
-  name: 'KKI-CTCL',
-  lifespan: {
-    // Days
-    accept: 420,
-    reject: 3
-  },
-  types: {
-    functional: { name:'_ga', value:'GA1.1.841501750.1638184465', status: false },
-    marketing: { status: false }
-  },
-  gaId: 'G-XVNYVMMPW9'
 }
 
 export default config
